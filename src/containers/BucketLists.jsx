45820080487b4adcb94f4252bf343f098469998c
Innocent Asiimwe <innocent@Innocents-MacBook-Pro.local>
2017-11-14 10:40:28 +0300

    Render fetched bucketlists

---
+++ src/containers/BucketLists.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import _ from 'lodash';
+import { getBucketlists } from '../actions/action_bucketlist';
+
+class BucketLists extends Component {
+  componentDidMount() {
+    this.props.getBucketlists();
+  }
+  renderBucketlists() {
+    return _.map(this.props.bucketlists, (bucketlist) => {
+      return (
+        <li className="list-group-item clearfix" key={bucketlist.id}>
+          <Link className="text-xs-left" to={`/bucketlists/${bucketlist.id}`}>
+            {bucketlist.name}
+          </Link>
+          <span className="pull-right">
+            <Link className="btn btn-success" to="#">
+                OPEN
+            </Link>
+            <Link className="btn btn-primary" to="#">
+                EDIT
+            </Link>
+            <Link className="btn btn-danger" to="#">
+                DELETE
+            </Link>
+          </span>
+        </li>
+      );
+    });
+  }
+  render() {
+    return (
+      <div className="jumbotron">
+        <h3>Bucketlists table</h3>
+        <ul className="list-group">
+          {this.renderBucketlists()}
+        </ul>
+      </div>
+
+    );
+  }
+}
+function mapStateToProps(state) {
+  return { bucketlists: state.bucketlists };
+}
+export default connect(mapStateToProps, { getBucketlists })(BucketLists);
