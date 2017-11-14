c82ccb0e10c37dde6f6f0a843f47badee186e7e8
Innocent Asiimwe <innocent@Innocents-MacBook-Pro.local>
2017-11-14 10:36:07 +0300

    Add reducer to handle successful fetch of bucketlists

---
+++ src/reducers/reducer_bucketlist.js
@@ -0,0 +1,11 @@
+import _ from 'lodash';
+import { GET_BUCKETLISTS_PENDING, GET_BUCKETLISTS_SUCCESS, GET_BUCKETLISTS_ERROR } from '../actions/action_bucketlist';
+
+export default function (state = [], action) {
+  switch (action.type) {
+    case GET_BUCKETLISTS_SUCCESS:
+      return _.mapKeys(action.payload.data, 'id');
+    default:
+      return state;
+  }
+}
