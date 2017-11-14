0e5dc6bd13e1130cd7a2a8925673babc843b6b55
Innocent Asiimwe <innocent@Innocents-MacBook-Pro.local>
2017-11-14 11:32:59 +0300

    Add action creator to add a new bucketlist

--- src/actions/action_bucketlist.js
+++ src/actions/action_bucketlist.js
@@ -4,11 +4,24 @@ export const GET_BUCKETLISTS = 'get_bucketlists';
 export const GET_BUCKETLISTS_PENDING = 'get_bucketlists_REQUEST';
 export const GET_BUCKETLISTS_SUCCESS = 'get_bucketlists_SUCCESS';
 export const GET_BUCKETLISTS_ERROR = 'get_bucketlists_ERROR';
+export const ADD_BUCKETLIST = 'add_bucketlist';
+export const ADD_BUCKETLIST_SUCCESS = 'add_bucketlist_SUCCESS';
+export const ADD_BUCKETLIST_PENDING = 'add_bucketlist_REQUEST';
+export const ADD_BUCKETLIST_ERROR = 'add_bucketlist_ERROR';
 
 export function getBucketlists() {
   const request = instance.get('/bucketlists');
   return {
     type: GET_BUCKETLISTS,
     payload: request
+  };
+}
+
+export function addBucketlist(values, callback) {
+  const request = instance.post('/bucketlists', values);
+  return {
+    type: ADD_BUCKETLIST,
+    payload: request,
+    callback: callback()
   };
 }
