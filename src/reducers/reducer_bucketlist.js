eb60e224f7148fe66dfc062dec526200ae8fc9e3
Innocent Asiimwe <innocent@Innocents-MacBook-Pro.local>
2017-11-14 17:25:15 +0300

    Add reducer for deleting bucketlist

--- src/reducers/reducer_bucketlist.js
+++ src/reducers/reducer_bucketlist.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
-import { GET_BUCKETLISTS_PENDING, GET_BUCKETLISTS_SUCCESS, GET_BUCKETLISTS_ERROR} from '../actions/action_bucketlist';
+import { GET_BUCKETLISTS_SUCCESS, DELETE_BUCKETLIST_SUCCESS } from '../actions/action_bucketlist';
 
 export default function (state = [], action) {
   switch (action.type) {
     case GET_BUCKETLISTS_SUCCESS:
       return _.mapKeys(action.payload.data, 'id');
+    case DELETE_BUCKETLIST_SUCCESS:
+      return _.omit(state, action.payload);
     default:
       return state;
   }
