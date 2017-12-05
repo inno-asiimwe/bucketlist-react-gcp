525772c8c729aea623d8bff97a7ac6df68353b6c
Innocent Asiimwe <innocent@Innocents-MacBook-Pro.local>
2017-12-05 12:11:37 +0300

    Add Snapshot test for Bucketlists

---
+++ src/__test__/containers/BucketLists.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
+
+import { BucketLists } from '../../containers/BucketLists';
+
+describe('Bucketlists', () => {
+  it('matches its snapshot', () => {
+    const output = shallow( <BucketLists getBucketlists={() => {}}/>)
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+});
