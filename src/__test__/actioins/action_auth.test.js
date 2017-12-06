efb81fae0f31d611128b50c70cb95d57155487c2
Innocent Asiimwe <innocent@Innocents-MacBook-Pro.local>
2017-12-06 17:57:27 +0300

    Add test for logoutUser action Creator

--- src/__test__/actioins/action_auth.test.js
+++ src/__test__/actioins/action_auth.test.js
@@ -43,4 +43,35 @@ describe('auth_actions', () => {
       expect(actionTypes).toEqual(actionsExpected);
     });
   });
+  it('dispatches login_REQUEST and login_SUCCESS', () => {
+    const credentials = { username: 'inno', password: 'pass' };
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200
+      });
+    });
+    const actionsExpected = ['login_REQUEST', 'login_SUCCESS'];
+    const store = mockStore({});
+    return store.dispatch(actions.loginUser(credentials)).then(() => {
+      const actionsDispatched = store.getActions();
+      const actionTypes = actionsDispatched.map(action => action.type);
+      expect(actionTypes).toEqual(actionsExpected);
+    });
+  });
+  it('dispatches logout_SUCCESS and logout_REQUEST', () => {
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200
+      });
+    });
+    const actionsExpected = ['logout_REQUEST', 'logout_SUCCESS'];
+    const store = mockStore({});
+    return store.dispatch(actions.logoutUser()).then(() => {
+      const actionsDispatched = store.getActions();
+      const actionTypes = actionsDispatched.map(action => action.type);
+      expect(actionTypes).toEqual(actionsExpected);
+    });
+  });
 });
