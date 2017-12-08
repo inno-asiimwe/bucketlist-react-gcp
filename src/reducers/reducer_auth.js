2eb1fbe96fb5c410dabd38b4113ce900847237a1
Innocent Asiimwe <innocent@Innocents-MacBook-Pro.local>
2017-12-08 11:36:51 +0300

    Add tests for auth reducers

--- src/reducers/reducer_auth.js
+++ src/reducers/reducer_auth.js
@@ -1,18 +1,23 @@
 import { REGISTER_SUCCESS, REGISTER_ERROR, REGISTER_PENDING, LOGIN_PENDING, LOGIN_SUCCESS, LOGIN_ERROR, LOGOUT_ERROR, LOGOUT_PENDING, LOGOUT_SUCCESS } from '../actions/action_auth';
 
-const initialState = {
+export const initialState = {
   Authenticated: false,
   loading: false,
   token: '',
   response: {}
 };
 
-export default function (state = initialState, action) {
+export default (state = initialState, action) => {
   switch (action.type) {
     case REGISTER_PENDING:
       return { ...state, loading: true };
     case REGISTER_SUCCESS:
-      return { ...state, response: action.payload.data };
+      return {
+        ...state,
+        response: action.payload.data,
+        loading: false,
+        loaded: true
+      };
     case REGISTER_ERROR:
       return { ...state, response: action.payload.response.data };
     case LOGIN_PENDING:
@@ -43,4 +48,4 @@ export default function (state = initialState, action) {
     default:
       return state;
   }
-}
+};
