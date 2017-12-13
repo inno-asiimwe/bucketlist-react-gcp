e38dabc1e5dc8fcb87307aa5c4049a8b3e0627c3
Innocent Asiimwe <innocent@Innocents-MacBook-Pro.local>
2017-12-13 16:47:36 +0300

    Add test for rendering a form

--- src/__test__/containers/NewBucketlistItem.test.js
+++ src/__test__/containers/NewBucketlistItem.test.js
@@ -6,7 +6,11 @@ import { NewItem } from '../../containers/NewBucketlistItem';
 
 describe('NewItem', () => {
   it('Matches its snapshot', () => {
-    const output = shallow(<NewItem match={{ params: { id: 1 } }} handleSubmit={() => {}}/>);
+    const output = shallow(<NewItem match={{ params: { id: 1 } }} handleSubmit={() => {}} />);
     expect(shallowToJson(output)).toMatchSnapshot();
+  });
+  it('renders a form', () => {
+    const component = shallow(<NewItem match={{ params: { id: 1 } }} handleSubmit={() => {}} />);
+    expect(component.find('form').length).toBe(1);
   });
 });
